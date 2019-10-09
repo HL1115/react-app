29fe4da233f5a5a7f98d24edf1536de739c18c92
HL1115 <1127570788@qq.com>
2019-10-09 14:21:25 +0800

    time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// react技术栈
+// jsx语法
+// var ele = <h1 class="tit">hello <p>react</p></h1>;
+// babel编译，返回一个对象
+// var ele = React.createElement(
+//     'h1',
+//     {id:'tit',class:'title'},
+//     'hello',
+//     React.createElement(
+//         'p',
+//         {id:'t',class:'tit'},
+//         'react')
+// );
+// ReactDOM.render(ele, document.getElementById('root'));
+// 自己封装render函数
 var obj = {
     type: 'div',
     props: {
@@ -24,6 +40,9 @@ var obj = {
 }
 function render(obj,container){
     var {type,props} = obj;
+    // 文档碎片
+    var fragment = document.createDocumentFragment();
+
     var ele = document.createElement(type);
     for(var item in props){
         if(item === 'class'){
@@ -42,19 +61,14 @@ function render(obj,container){
             ele[item] = props[item];
         }
     }
-    container.appendChild(ele);
+    fragment.appendChild(ele);
+    container.appendChild(fragment);
 }
-render(obj,document.getElementById('root'));
-
-console.log('1');
-// var ele = <h1 class="tit">hello <p>react</p></h1>;
-// var ele = React.createElement(
-//     'h1',
-//     {id:'tit',class:'title'},
-//     'hello',
-//     React.createElement(
-//         'p',
-//         {id:'t',class:'tit'},
-//         'react')
-// );
-// ReactDOM.render(ele, document.getElementById('root'));
+// render(obj,document.getElementById('root'));
+// 页面回流（重排reflow）：DOM结构变化、内容变化、大小、位置的变化
+// 页面重绘（repaint）：颜色的变化（背景色、字体颜色、边框颜色）
+console.time('time');
+for(var i=0;i<1000;i++){
+    document.getElementById('root').innerHTML += '<p>'+i+'</p>';
+}
+console.timeEnd('time');
