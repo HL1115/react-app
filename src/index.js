c4cde125baf2531e06c57236c0132a2c05f8803b
HL1115 <1127570788@qq.com>
2019-10-09 14:50:12 +0800

    基础

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 // react技术栈
 // jsx语法
 // var ele = <h1 class="tit">hello <p>react</p></h1>;
@@ -65,10 +65,34 @@ function render(obj,container){
     container.appendChild(fragment);
 }
 // render(obj,document.getElementById('root'));
-// 页面回流（重排reflow）：DOM结构变化、内容变化、大小、位置的变化
+// 页面渲染过程
+// 请求HTML页面、浏览器HTML解析器解析html文件、生成DOM树
+// link引入css文件、css解析器解析CSS，生成CSS对象模型，CSSOM和DOM tree结合生成一个render tree，最后浏览器绘制页面
+
+// 页面回流（重排reflow）：DOM结构变化、内容变化、大小、位置的变化、显示的变化
 // 页面重绘（repaint）：颜色的变化（背景色、字体颜色、边框颜色）
-console.time('time');
-for(var i=0;i<1000;i++){
-    document.getElementById('root').innerHTML += '<p>'+i+'</p>';
-}
-console.timeEnd('time');
+
+// 1、先用变量进行dom处理，最后一次渲染
+// console.time('time');
+// var div = document.getElementById('root');
+// var str = ''
+// for(var i=0;i<1000;i++){
+//     str += '<p>'+i+'</p>';
+// }
+// div.innerHTML = str;
+// console.timeEnd('time');
+// 2、对于样式处理,声明一个css类
+var div = document.getElementById('root');
+// div.style.width = '100px';
+// div.style.height = '100px';
+// div.style.background = 'red';
+
+div.className = 'active';
+// 3、offsetLeft、offsetWidth等都会引起回流，要慎用
+// console.log(div.offsetLeft);
+// var wid = div.offsetWidth;
+// setInterval(()=>{
+//     wid += 1;
+//     div.style.width = wid + 'px';
+// },100)
+// 4、文档碎片（内存中的一个变量）
