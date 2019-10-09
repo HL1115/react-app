3f96bf06ad930c547ed88897ef639973f176ad53
HL1115 <1127570788@qq.com>
2019-10-09 12:46:45 +0800

    f

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+var obj = {
+    type: 'div',
+    props: {
+        id: 'box',
+        class: 'box',
+        children: [
+            'hello',
+            'react',
+            {
+                type: 'h1',
+                props: {
+                    id: 'tit',
+                    class: 'tit',
+                    children: [
+                        'title',
+                        'react'
+                    ]
+                }
+            }
+        ]
+    }
+}
+function render(obj,container){
+    var {type,props} = obj;
+    var ele = document.createElement(type);
+    for(var item in props){
+        if(item === 'class'){
+            ele.className = props[item]
+        }else if(item === 'children'){
+            for(var i=0;i<props.children.length;i++){
+                // ele.innerHTML += props.children[i];
+                if(typeof props.children[i] === 'object'){
+                    render(props.children[i],ele);
+                }else{
+                    var txt = document.createTextNode(props.children[i]);
+                    ele.appendChild(txt);
+                }
+            }
+        }else{
+            ele[item] = props[item];
+        }
+    }
+    container.appendChild(ele);
+}
+render(obj,document.getElementById('root'));
+
+
+// var ele = <h1 class="tit">hello <p>react</p></h1>;
+// var ele = React.createElement(
+//     'h1',
+//     {id:'tit',class:'title'},
+//     'hello',
+//     React.createElement(
+//         'p',
+//         {id:'t',class:'tit'},
+//         'react')
+// );
+// ReactDOM.render(ele, document.getElementById('root'));
