0c8c978c6e5ae7f1176fb439f7e1b6772928bf97
HL1115 <1127570788@qq.com>
2019-10-16 16:50:57 +0800

    antd

--- src/Sider.js
+++ src/Sider.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import {Button} from 'antd';
 export default class Sider extends Component {
     render() {
         let style = {float: 'left'}
         return (
             <ul style={style}>
+                <Button type="primary" block>按钮</Button>
                 <li>
                     <Link to='/hoc'>Hoc</Link>
                 </li> 
