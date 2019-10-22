d0c32558c0fd349faa0ddb8fc037d8271a2a3af0
HL1115 <1127570788@qq.com>
2019-10-22 15:33:24 +0800

    life

--- src/Router/Content.js
+++ src/Router/Content.js
@@ -12,17 +12,31 @@ export default class Content extends React.Component{
         fetch('https://cnodejs.org/api/v1/topics?page='+page)
             .then((res)=>res.json())
             .then((res)=>{
+                console.log(res)
                 this.setState({
                     data: res.data
                 });
             })
+    }
+    componentDidUpdate(prevProps,prevState){
+        if(prevProps.match.params.id!==this.props.match.params.id){
+            let page = this.props.match.params.id;
+            fetch('https://cnodejs.org/api/v1/topics?page='+page)
+                .then((res)=>res.json())
+                .then((res)=>{
+                    console.log(res)
+                    this.setState({
+                        data: res.data
+                    });
+                })
+        }
     }
     render(){
         return (
             <div>
                 {
                     this.state.data.map((item)=>(
-                        <div key={item.id}>
+                        <div style={{border:'2px solid green',width:500,height:200,overflow:'hidden'}} key={item.id}>
                             <h2>{item.title}</h2>
                             <div dangerouslySetInnerHTML={{__html:item.content}}></div>
                         </div>
