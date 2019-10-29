9f4a73b3caf03d591a5b6ecb7f66c4849091b042
HL1115 <1127570788@qq.com>
2019-10-29 16:48:24 +0800

    switch

--- src/react-router-dom/Switch.js
+++ src/react-router-dom/Switch.js
@@ -12,7 +12,11 @@ export default class Switch extends Component {
                         let children = this.props.children;
 
                         for(var i=0;i<children.length;i++){
-                            let reg = pathToReg(children[i].props.path)
+                            let reg = pathToReg(
+                                children[i].props.path,
+                                [],
+                                {end:children[i].props.exact||false}
+                            )
                             if(reg.test(pathname)){
                                 return children[i]
                             }
