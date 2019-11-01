b7281d4d2ba746219609d157f76e22f3dd00c6e5
HL1115 <1127570788@qq.com>
2019-11-01 16:10:33 +0800

    react-redux

--- src/containers/Todoing.js
+++ src/containers/Todoing.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react'
-import store from '../store';
 import {DEL_ITEM} from '../actions/actionTypes';
 import {connect} from 'react-redux';
 
-class Todoing extends Component {
-    delItem = (index)=>{
-        this.props.dispatch({
+// class Todoing extends Component {
+//     delItem = (index)=>{
+//         this.props.dispatch({
+//             type: DEL_ITEM,
+//             index
+//         })
+//     }
+//     render() {
+//         console.log(this.props)
+//         return (
+//             <ul>
+//                 {
+//                     this.props.todos.map((item,idx)=>(
+//                         <li key={idx}>
+//                             {item}----
+//                             <button onClick={()=>{this.delItem(idx)}}>删除</button>
+//                         </li>
+//                     ))
+//                 }
+//             </ul>
+//         )
+//     }
+// }
+function Todoing({dispatch,todos}){
+    let delItem = (index)=>{
+        dispatch({
             type: DEL_ITEM,
             index
         })
     }
-    render() {
-        console.log(this.props)
-        return (
-            <ul>
-                {
-                    this.props.todos.map((item,idx)=>(
-                        <li key={idx}>
-                            {item}----
-                            <button onClick={()=>{this.delItem(idx)}}>删除</button>
-                        </li>
-                    ))
-                }
-            </ul>
-        )
-    }
+    return (
+        <ul>
+            {
+                todos.map((item,idx)=>(
+                    <li key={idx}>
+                        {item}----
+                        <button onClick={()=>{delItem(idx)}}>删除</button>
+                    </li>
+                ))
+            }
+        </ul>
+    )
 }
 
 let mapStateToProps = (state)=>{
